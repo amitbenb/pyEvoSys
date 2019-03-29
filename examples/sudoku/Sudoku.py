94a75e643deac35c5de2e7301e0a4ea838f503d4
amitbenb <SilentMike0@gmail.com>
2019-03-29 22:23:30 +0300

    Sudoku implemented. Seems to work.

---
+++ examples/sudoku/Sudoku.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+class Sudoku:
+    def __init__(self, board=None):
+        if board is None:
+            self.board = np.zeros((4, 4), int)
+        else:
+            self.board = np.array(board)
+        self.size = len(self.board)
+
+    def get_elements(self):
+        board = self.board
+        size = self.size
+        rs = int(np.sqrt(size))  # Root of size
+        elements = np.array([i for i in board] + [i for i in board.transpose()])
+        square_elements = np.array(
+            [[board[(i // rs) + (rs * (j // rs))][(i % rs) + (rs * (j % rs))]
+              for i in range(size)]
+             for j in range(size)])
+        elements = np.vstack((elements, square_elements))
+        return elements
+
+    def evaluate_board(self):
+        ret_val = {'mistake_count': 0, 'bad_elements': 0}
+        elements = self.get_elements()
+        size = self.size
+        for e in elements:
+            number_of_duplicates = size - len(set(e).intersection(set(range(1, size+1))))
+            ret_val['mistake_count'] += number_of_duplicates
+            if number_of_duplicates > 0:
+                ret_val['bad_elements'] += 1
+        return ret_val
+
+
+if __name__ == "__main__":
+    _board = np.array(np.random.permutation((np.arange(1,17) % 4) + 1)).reshape(4, 4)
+    # _board = np.array(list(range(1, 82))).reshape(9, 9)
+    print(_board)
+    print()
+    s = Sudoku(_board)
+    print(s.get_elements())
+    print()
+    print(s.evaluate_board())
+
