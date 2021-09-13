977bc0da9fc48c246ccf2e8da4fae80338728920
Amit <SilentMike0@gmail.com>
2021-09-14 00:43:27 +0300

    array-gp package and sentence_scoring package started.

---
+++ examples/sentence_scoring/__init__.py
@@ -0,0 +1,9 @@
+"""
+Domain for (Array) GP
+Terminals are words (all words in sentences)
+Functions are punctuation and spaces.
+Set of many scoring sentences (e.g. 'I have an apple', 'you like fish'...).
+len fitness points for first time Individual makes a sentence (/2 for second time /4 for third, etc).
+Make as
+"""
+
